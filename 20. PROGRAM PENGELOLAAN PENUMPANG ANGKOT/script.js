024ef45c2291a8cbf9af870d21a35acd9cf0ab0c
akhilesshyantica <akhilesshyanticaw@gmail.com>
2024-03-22 20:41:37 +0700

    menambahkan komentar pada setiap bagian program

--- 20. PROGRAM PENGELOLAAN PENUMPANG ANGKOT/script.js
+++ 20. PROGRAM PENGELOLAAN PENUMPANG ANGKOT/script.js
@@ -1,18 +1,28 @@
 var penumpang = ["Akhiles", undefined, "Wisnumurti"];
 var tambahPenumpang = function(namaPenumpang, penumpang){
+    // jika angkor kosong
     if(penumpang.length == 0){
+        // tambah penumpang di awal array
         penumpang.push(namaPenumpang);
+        // kembalikan isi array dan keluar dari function
         return penumpang;
     }
     else{
+        // telusuri seluruh kursi dari awal
         for(var i = 0; i < penumpang.length; i++){
+            // jika ada kursi kosong
             if(penumpang[i] == undefined){
+                // tambah penumpang di kursi tersebut
                 penumpang[i] = namaPenumpang;
+                // kembalikan isi array & keluar dari function
                 return penumpang;
             }
-            else{
-                
-            }
+            // jika sudah ada nama yang sama
+                // tampilkan pesan kesalahannya
+                // kembalikan isi array & keluar dari function
+            // jika seluruh kursi terisi
+                // tambah penumpang di akhir array
+                // kembalikan isi array & keluar dari function
         }
     }
 }
