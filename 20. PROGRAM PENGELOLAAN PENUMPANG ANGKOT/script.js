221a32b5733479aff4e0bec64ba0d4f30aac887f
akhilesshyantica <akhilesshyanticaw@gmail.com>
2024-03-22 21:38:42 +0700

    menyelesaikan program.

--- 20. PROGRAM PENGELOLAAN PENUMPANG ANGKOT/script.js
+++ 20. PROGRAM PENGELOLAAN PENUMPANG ANGKOT/script.js
@@ -1,4 +1,4 @@
-var penumpang = ["Akhiles", undefined, "Wisnumurti"];
+var penumpang = [];
 var tambahPenumpang = function(namaPenumpang, penumpang){
     // jika angkor kosong
     if(penumpang.length == 0){
@@ -36,16 +36,31 @@ var tambahPenumpang = function(namaPenumpang, penumpang){
         }
     }
 }
+
 var hapusPenumpang = function(namaPenumpang, penumpang){
     // jika angkot kosong
         // tampilkan pesan bahwa angkot kosong dan tidak mungkin ada penumpang turun
+    if(penumpang.length == 0){
+        console.log("Angkot masih kosong.");
         // kembalikan isi array & keluar dari function
-    // else
+        return penumpang;
+    } else {
         // telusuri seluruh kursi dari awal
-            // jika nama penumpang sesuai
+        for(var i = 0; i < penumpang.length; i++){
+             // jika nama penumpang sesuai
                 // hapus penumpang dengan mengubah namanya menjadi undefined
-                // kembalikan isi array & keluar dari function
-            // jika tidak ada nama yang sesuai
+            if(penumpang[i] == namaPenumpang){
+                penumpang[i] = undefined;
+                 // kembalikan isi array & keluar dari function
+                return penumpang;
+            }
+            else if( i == penumpang.length - 1){
+                    // jika tidak ada nama yang sesuai
                 // tampilkan pesan kesalahannya
-                // kembalikan isi array & keluar dari function
+                console.log(namaPenumpang + " tidak ada di dalam angkot ini");
+                 // kembalikan isi array & keluar dari function
+                return penumpang;
+            }
+        }
+    }           
 }
