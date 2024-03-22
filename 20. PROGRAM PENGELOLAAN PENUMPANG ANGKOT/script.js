ea14caefece8d3a227a266dec9b444f64a924446
akhilesshyantica <akhilesshyanticaw@gmail.com>
2024-03-22 20:16:12 +0700

    inisialisasi awal, push namaPenumpang dan namaPenumpang masuk jika undefined.

---
+++ 20. PROGRAM PENGELOLAAN PENUMPANG ANGKOT/script.js
@@ -0,0 +1,18 @@
+var penumpang = ["Akhiles", undefined, "Wisnumurti"];
+var tambahPenumpang = function(namaPenumpang, penumpang){
+    if(penumpang.length == 0){
+        penumpang.push(namaPenumpang);
+        return penumpang;
+    }
+    else{
+        for(var i = 0; i < penumpang.length; i++){
+            if(penumpang[i] == undefined){
+                penumpang[i] = namaPenumpang;
+                return penumpang;
+            }
+            else{
+                
+            }
+        }
+    }
+}
